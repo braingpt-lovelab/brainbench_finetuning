8788f215ece593626becec593ba11a324937bb4a
Ubuntu <ken@braingptbenchmark.ycq3ohurltmuvcsgktwuxjhoib.zrhx.internal.cloudapp.net>
2023-12-21 19:28:27 +0000

    add code: tokenizer loader

--- data/utils.py
+++ data/utils.py
@@ -7,3 +7,11 @@ def doi_reformer(doi):
     doi = doi.replace("(", "=")
     doi = doi.replace(")", "=")
     return doi
+
+
+def load_tokenizer(model_fpath):
+    import transformers
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        model_fpath,
+    )
+    return tokenizer
