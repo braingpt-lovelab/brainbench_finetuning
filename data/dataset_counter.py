2aa0f3a588e0c565c0e805cf4d2e5c1455ddfb14
Ubuntu <ken@braingptbenchmark.ycq3ohurltmuvcsgktwuxjhoib.zrhx.internal.cloudapp.net>
2023-12-21 13:08:01 +0000

    add code: count file numbers

---
+++ data/dataset_counter.py
@@ -0,0 +1,36 @@
+import os
+import json
+
+
+def count_files():
+    """
+    Count the number of files in each dir.
+    """
+    # Read all journal names from `journal_names.json`
+    # For each journal, look into dir `dataset/{journal}/`
+    # Seperately look into
+    # `dataset/{journal}/abstracts/` and `dataset/{journal}/fulltext/`
+    # And count the number of files in each dir
+    with open("journal_names.json", "r") as f:
+        journal_names = json.load(f)
+
+    total_abstracts_count = 0
+    total_fulltext_count = 0
+    for journal in journal_names["journal_names"]:
+        abstracts_dir = os.path.join(f"dataset/{journal}", "abstracts")
+        fulltext_dir = os.path.join(f"dataset/{journal}", "fulltext")
+
+        abstracts_count = len(os.listdir(abstracts_dir))
+        fulltext_count = len(os.listdir(fulltext_dir))
+
+        total_abstracts_count += abstracts_count
+        total_fulltext_count += fulltext_count
+
+        print(f"[{journal}]: abstracts [{abstracts_count}], fulltext [{fulltext_count}]")
+    
+    print(f"Total abstracts: [{total_abstracts_count}]")
+    print(f"Total fulltext: [{total_fulltext_count}]")
+
+
+if __name__ == "__main__":
+    count_files()
