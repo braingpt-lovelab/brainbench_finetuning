b739a5ae5e4410ec8a3d353437bf37347f7524ef
Ubuntu <ken@braingptbenchmark.ycq3ohurltmuvcsgktwuxjhoib.zrhx.internal.cloudapp.net>
2023-12-21 18:23:00 +0000

    add code: print info about zero counts

--- data/dataset_counter.py
+++ data/dataset_counter.py
@@ -16,12 +16,18 @@ def count_files():
 
     total_abstracts_count = 0
     total_fulltext_count = 0
+    zero_collector = []
     for journal in journal_names["journal_names"]:
         abstracts_dir = os.path.join(f"dataset/{journal}", "abstracts")
         fulltext_dir = os.path.join(f"dataset/{journal}", "fulltext")
 
         abstracts_count = len(os.listdir(abstracts_dir))
         fulltext_count = len(os.listdir(fulltext_dir))
+
+        if abstracts_count == 0 or fulltext_count == 0:
+            zero_collector.append(
+                f"{journal}: abstracts [{abstracts_count}], fulltext [{fulltext_count}]"
+            )
 
         total_abstracts_count += abstracts_count
         total_fulltext_count += fulltext_count
@@ -30,6 +36,8 @@ def count_files():
     
     print(f"Total abstracts: [{total_abstracts_count}]")
     print(f"Total fulltext: [{total_fulltext_count}]")
+    for journal in zero_collector:
+        print(journal)
 
 
 if __name__ == "__main__":
